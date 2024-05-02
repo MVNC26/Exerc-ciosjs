e1a1ad717ab8ebeef4a775c9f33966d1d12fbebb
MVNC26 <djavuaxebahia@gmail.com>
2024-05-02 17:01:30 -0300

    feat: exercicio js 1

---
+++ exercicio1.js
@@ -0,0 +1,29 @@
+//Escreva um programa que solicita ao usuário um nome de usuário e uma senha. 
+// Verifique se o nome de usuário é "admin" e a senha é "senha123". Exiba uma mensagem indicando se o login foi bem-sucedido ou não.
+
+
+//prompt -> recebe uma entrada digitada
+let nome = prompt ("olá, digite seu nome:")
+
+//concatenação (+)
+console.log ("seja bem vindo " + nome);
+if (nome != "adimin" ){
+    alert ("o nome está incorreto!!")
+}
+
+let senha = parseFloat ( prompt ("Digite a sua senha:"))
+console.log ("aproveite adimin")
+ if(senha != "senha123"){
+    alert ("a senha digitada está incorreta")
+ }
+
+
+
+//let pontos =100
+//let bonus = paraseint (prompt ("quantos pontos você deseja somar"))
+//console log (pontos + bonus)
+
+
+
+
+
