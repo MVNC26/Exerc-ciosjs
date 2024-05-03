c19b236cb8cb22b1b7ea4137a5460c1eeed655de
MVNC26 <djavuaxebahia@gmail.com>
2024-05-03 15:35:39 -0300

    feat: exercicio2 js

--- exercicio1.js
+++ exercicio1.js
@@ -1,19 +1,6 @@
-//Escreva um programa que solicita ao usuário um nome de usuário e uma senha. 
-// Verifique se o nome de usuário é "admin" e a senha é "senha123". Exiba uma mensagem indicando se o login foi bem-sucedido ou não.
+//Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana e exibe o nome 
+//correspondente a esse dia. Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante.
 
-
-//prompt -> recebe uma entrada digitada
-let username = prompt ("Digite um nome de usuário")
-let senha = prompt ("Digite a senha:")
-
-//concatenação (+), ==-> verifica se o valor é igual a outro, === -> verifica se o valor r o tipo dos itens são iguais
-if (username == "admin") {
-    console.log ("voce é admin");
-    
-    if (senha === "senha123") {
-        console.log("bem vindo ADMIN!");
-    }
-}
 
 
 
---
+++ exercicio2.js
@@ -0,0 +1,32 @@
+//Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da semana e exibe o nome 
+//correspondente a esse dia. Considere que 1 representa segunda-feira, 2 representa terça-feira e assim por diante.
+
+let dia = parseInt(prompt("digite um número de 1 a 7") )
+switch (dia) {
+    case 1: 
+        console.log ("segunda-feira")
+        break;
+    case 2: 
+        console.log ("terça-feira")
+        break;
+    case 3: 
+        console.log ("quarta-feira")
+        break;
+    case 4: 
+        console.log ("quinta-feira")
+        break;
+    case 5: 
+        console.log ("sexta-feira")
+        break;
+    case 6: 
+        console.log ("Sabádo")
+        break;
+    case 7: 
+        console.log ("Domingo")
+        break;
+    default:
+        console.log ("número digitado errado!!")
+
+   
+
+}
