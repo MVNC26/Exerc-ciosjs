30d2635ca27846bbef1cb3e97a472bd8de55924f
MVNC26 <djavuaxebahia@gmail.com>
2024-05-03 15:45:08 -0300

    feat: exercicio 3 js

---
+++ exercicio3.js
@@ -0,0 +1,9 @@
+//Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
+
+let n = parseInt(prompt("digite um número!"))
+ if (n%2 == 0) {
+    console.log ("o número "+ n + " é par")
+ }
+ else{
+    console.log ("o número "+ n + " é ímpar")
+ }
