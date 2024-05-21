08edad73920138a732958967991bc507d1f62b5a
MVNC26 <djavuaxebahia@gmail.com>
2024-05-21 17:10:33 -0300

    feat: exercicio 10 feito

---
+++ for_while/exercicio10.js
@@ -0,0 +1,10 @@
+// Faça um programa que receba um número do usuário e continue a pedir o número se o numero for diferente de 0.
+
+
+while (true) {
+    let numero = prompt("digite um numero?")
+    if ( numero == 0) {
+        console.log ("você digitou 0 !!")  
+        break  
+    }
+}
