acae5f198200d126eee6487a76c9d7eecb0c8a8d
MVNC26 <djavuaxebahia@gmail.com>
2024-05-21 16:37:52 -0300

    feat: exercicio 7  feito

---
+++ for_while/exercicio7.js
@@ -0,0 +1,11 @@
+// Calcular a soma dos números de 1 a 100 utilizando a estrutura while.
+
+let numero = 1
+let total = 0
+
+while (numero <= 100) {
+    total += numero
+    numero ++ 
+}
+
+console.log (total)
